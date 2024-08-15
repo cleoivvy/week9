c503287c76307c3d4e637b78d2ab29b360c938af
cleoivvy <claraojobo@gmail.com>
2024-08-15 12:39:07 +0100

    fix:update the date

--- src/app/about/components/Footer.jsx
+++ src/app/about/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='h-36 bg-slate-950 flex items-center justify-center text-white'>
-      <p>&copy; Established {(new Date('26-10-2001').getFullYear())} -{(new Date().getFullYear())}</p> 
+      <p>&copy; Established {(new Date('10-26-2001').getFullYear())} -{(new Date().getFullYear())}</p> 
     </footer>
   )
 }

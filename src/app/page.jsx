ede0c8c92043407e7e4769433b61fe518b277835
cleoivvy <claraojobo@gmail.com>
2024-08-15 12:30:39 +0100

    fix:change apsotrophe

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='bg-slate-900 h-screen flex flex-col text-center justify-center text-white font-bold text-4xl'>
-        <h1>Clara's first React</h1>
+        <h1>Clara&apos;s first React</h1>
     </div>
   )
 }
